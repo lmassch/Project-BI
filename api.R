f8a680401bd9cc7e26761dd68d88ccba75abeb33
wagnerrovira <clara.wagner@estudiantat.upc.edu>
2024-12-08 16:06:52 +0100

    Add files via upload

--- api.R
+++ api.R
@@ -1,57 +1,60 @@
-# Cargar librerías necesarias
-
-rm(list = ls())  # Limpia todas las variables de tu entorno actual
-gc()  # Limpia la memoria
-
-if ("plumber" %in% loadedNamespaces()) {
-  detach("package:plumber", unload = TRUE)
-}
-
-if ("plumber" %in% rownames(installed.packages())) {
-  remove.packages("plumber")
-}
-
-install.packages("plumber", dependencies = TRUE, show_col_types = FALSE)
+#* @apiTitle Breast Cancer Prediction
+#* @apiDescription Hackaton IB 
 library(plumber)
-library(tidyverse)
-
-# Cargar el modelo previamente entrenado
-model <- readRDS("C:/Users/wagne/Documents/UNI/MÀSTER/Q3/IB/PROJECTE/logistic_model.rds")
-model_data <- read_csv("C:/Users/wagne/Documents/UNI/MÀSTER/Q3/IB/PROJECTE/shuffled_combined_data.csv")  
-
-# Create a new plumber router
-pr <- plumber::plumber$new()
-
-
-pr$handle("POST", "/predict", function(req, BRCA_GENE, AGE, INFERRED_MENOPAUSAL_STATE, HORMONE_THERAPY) {
-
-  if (any(is.na(c(BRCA_GENE, AGE, INFERRED_MENOPAUSAL_STATE, HORMONE_THERAPY)))) {
-    res$status <- 400
-    return(list(error = "Todos los parámetros son obligatorios."))
-  }
-  
-  # Dataframe con los parámetros del usuario
+library(ggplot2)
+library(dplyr)
+library(readr)
+#model <- readRDS("C:/Users/wagne/Documents/UNI/MÀSTER/Q3/IB/PROJECTE/logistic_model.rds")
+model_data <- read_csv("C:/Users/wagne/Documents/UNI/MÀSTER/Q3/IB/PROJECTE/shuffled_combined_data.csv") 
+#* @param BRCA_GENE The state of the BRCA gene (1 for mutated, 0 for not mutated)
+#* @param HORMONE_THERAPY Whether the patient receives hormone therapy (1 for yes, 0 for no)
+#* @param INFERRED_MENOPAUSAL_STATE Inferred menopausal state (1 for postmenopausal, 0 for premenopausal)
+#* @param AGE The age of the patient 
+#* @serializer json
+#* @get /predict
+function(BRCA_GENE, HORMONE_THERAPY, INFERRED_MENOPAUSAL_STATE, AGE) {
+  model <- glm(CANCER ~ BRCA_GENE + INFERRED_MENOPAUSAL_STATE + HORMONE_THERAPY + AGE, 
+               family = binomial, 
+               data = model_data)
   input_data <- data.frame(
-    BRCA_GENE = as.numeric(BRCA_GENE),
-    AGE = as.numeric(AGE),
-    INFERRED_MENOPAUSAL_STATE = as.numeric(INFERRED_MENOPAUSAL_STATE),
-    HORMONE_THERAPY = as.numeric(HORMONE_THERAPY)
+    BRCA_GENE = as.numeric(model_data$BRCA_GENE),
+    AGE = as.numeric(model_data$AGE),
+    INFERRED_MENOPAUSAL_STATE = as.numeric(model_data$INFERRED_MENOPAUSAL_STATE),
+    HORMONE_THERAPY = as.numeric(model_data$HORMONE_THERAPY)
   )
-  
-  # Predecir probabilidades
   prediction <- predict(model, newdata = input_data, type = "response")
-  
-  # Devolver la probabilidad y clase predicha
-  res <- list(
-    cancer_probability = prediction,
+  list(
+    cancer_probability = round(prediction*100,2),
     cancer_status = ifelse(prediction > 0.5, "Cancer", "Healthy")
   )
-  
-  return(res)
-}
-
-# Inicia el servidor
-
-pr$run(port = 8000)
-
+} 
+#* @param BRCA_GENE The state of the BRCA gene (1 for mutated, 0 for not mutated)
+#* @param HORMONE_THERAPY Whether the patient receives hormone therapy (1 for yes, 0 for no)
+#* @param INFERRED_MENOPAUSAL_STATE Inferred menopausal state (1 for postmenopausal, 0 for premenopausal)
+#* @param AGE The age of the patient 
+#* @serializer png
+#* @get /cancer-probability-plot
+function(BRCA_GENE, INFERRED_MENOPAUSAL_STATE, HORMONE_THERAPY) {
+  # Filtrar los datos existentes
+  filtered_data <- model_data %>%
+    filter(
+      BRCA_GENE == model_data$BRCA_GENE,
+      INFERRED_MENOPAUSAL_STATE == model_data$INFERRED_MENOPAUSAL_STATE,
+      HORMONE_THERAPY == model_data$HORMONE_THERAPY
+    )
+  }
+  filtered_data$CANCER_PROBABILITY <- predict(model, newdata = filtered_data, type = "response")
+  plot <- ggplot(filtered_data, aes(x = AGE, y = CANCER_PROBABILITY)) +
+    geom_point(color = "blue", size = 2) +
+    geom_smooth(method = "loess", color = "red") +
+    labs(
+      title = "Probabilidad de Cáncer según la Edad",
+      x = "Age",
+      y = "Probability of cancer"
+    ) +
+    theme_minimal()
+  # Guardar la gráfica como imagen temporal
+  tmp <- tempfile(fileext = ".png")
+  ggsave(tmp, plot = plot, device = "png", width = 8, height = 6)
+  readBin(tmp, "raw", n = file.info(tmp)$size)
 
